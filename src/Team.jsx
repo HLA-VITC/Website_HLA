60a7259b3b9dea1d8aeab6f8913e29c3cdee72be
Shashank <shashwritescode@gmail.com>
2023-06-14 01:49:54 +0530

    Added Event Section

--- src/Team.jsx
+++ src/Team.jsx
@@ -1,14 +1,9 @@
 import React from "react";
 
-function Team() {
+export default function Team() {
   return (
-    <section className="colored-section">
-      <div className="Board-carousel">
-        <h2 className="Member's-text">Lorem Ipsum</h2>
-        <img className="Member's-img" href="" alt="event-poster" />
-      </div>
-    </section>
+    <div>
+      <h1>Our Team</h1>
+    </div>
   );
 }
-
-export default Team;
