fc581908428da10bae7e20c69a719f4f250f726d
Medhansh Verma <88682701+Medhanshverma@users.noreply.github.com>
2023-06-22 13:51:32 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,40 @@
 import React from "react";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import Event from "./Event";
+import { eventData, responsive } from "./eventData";
+
 import Navbar from "./Navbar";
 import Backimg from "./Backimg";
 import About from "./About";
-function App() {
+import Team from "./Team";
+import Foot from "./Foot";
+
+
+export default function App() {
+  const events = eventData.map((item) => (
+    <Event
+      name={item.name}
+      url={item.imageurl}
+      date={item.date}
+      description={item.description}
+    />
+    ));
+
   return (
     <div>
       <Navbar/>
       <Backimg/>
       <About/>
+      <Team />
+      <div className="event">
+      <h1>Our Events</h1>
+      <Carousel showDots={false} responsive={responsive}>
+        {events}
+      </Carousel>
+      </div>
+      <Foot />
     </div>
   );
 }
 
-export default App;
